410a9bbf5987421fb94a7a56f2ac69cdc8411103
vikas <vickyvsvirat@gmail.com>
2023-12-02 18:32:27 +0530

    change in footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,29 @@
-import { Box, Image, Text} from '@chakra-ui/react'
+import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import btcSrc from '../assets/btc-img.png'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
     <Box w={'full'} h={'85vh'} bgColor={'blackAlpha.900'}>
-       <motion.div
+      <motion.div
         style={{
-        height:'80vh'
-       }} 
-       animate={{
-        translateY:'20px'
-       }}
-       transition={
-        {
-          duration: 2,
-          repeat: Infinity,
-          repeatType: 'reverse'
+          height: '80vh'
+        }}
+        animate={{
+          translateY: '20px'
+        }}
+        transition={
+          {
+            duration: 2,
+            repeat: Infinity,
+            repeatType: 'reverse'
+          }
         }
-       }
-       >
-       <Image w={'full'} h={'full'} objectFit={'contain'} filter = {'grayscale(1)'} src={btcSrc}/>
-       </motion.div>
-       <Text fontSize={'6xl'} textAlign={'center'} color={'whiteAlpha.700'} fontWeight={'thin'} mt={'-10'}>Xcrypto</Text>
+      >
+        <Image w={'full'} h={'full'} objectFit={'contain'} filter={'grayscale(1)'} src={btcSrc} />
+      </motion.div>
+      <Text fontSize={'6xl'} textAlign={'center'} color={'whiteAlpha.700'} fontWeight={'thin'} mt={'-10'}>Xcrypto</Text>
     </Box>
   )
 }
